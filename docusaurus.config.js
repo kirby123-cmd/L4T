1612b477db1e0d07e13d757477954450b9844d5e
kirby123-cmd <kirubaselvi40805@gmail.com>
2022-09-24 23:07:08 +0530

    version 1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,16 +8,16 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "L4T",
   tagline: "Language For Training",
-  url: "https://your-docusaurus-test-site.com",
-  baseUrl: "/",
+  url: "https://kirby123-cmd.github.io",
+  baseUrl: "/L4T",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "kirby123-cmd", // Usually your GitHub org/user name.
+  projectName: "L4T", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
